c7f8b8dbe6c04bff3a8ada41b9af9b6e658ba0ac
pitabas prathal <pitabas.prathal@gmail.com>
2018-01-09 18:11:31 +0530

    build added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { RoutingModule } from './modules/routing/routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './containers/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
